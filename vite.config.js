23d32e40eeecf7d5f1457e4fe4cdf418dbdcc265
ithe2k <ithe2k@gmail.com>
2025-11-24 00:28:46 +0100

    Update vite.config.js para solucionar problema de rotura de rutas en github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "./", //para el uso de enlaces relativos en enlaces generados en el html
+   base: '/CSS-PROJECT__Conquer-Academy/', //para el uso de enlaces relativos en enlaces generados en el html
     server: {
     // Esto es lo que le dice a Vite que abra el navegador al arrancar
     open: true, 
